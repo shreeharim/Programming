2721198a9ccc931e6434d946cf8b06233a7256f9
root <root@nsshayan>
2017-12-27 22:20:11 +0530

    cpp

---
+++ cpp/class_practice.cpp
@@ -0,0 +1,27 @@
+//#include <iostream.h>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class geeks
+{
+    public:
+    string geekname;
+
+    void printname()
+    {
+        count << "Geekname is:" << geekname;
+    }
+};
+
+int main()
+{
+    geeks obj1;
+
+    obj.geekname = Shreehari;
+
+    obj.printname();
+    return 0;
+
+}
+
